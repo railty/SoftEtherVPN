8ed2b01d13c4be813578e9cd18bd1e7b74fbf9ea
shawn ning <zxning@gmail.com>
2018-06-24 22:57:53 -0400

    update

--- src/vpnacct/vpnacct.c
+++ src/vpnacct/vpnacct.c
@@ -1,32 +1,32 @@
 // SoftEther VPN Source Code - Stable Edition Repository
 // Cedar Communication Module
-//
+// 
 // SoftEther VPN Server, Client and Bridge are free software under GPLv2.
-//
+// 
 // Copyright (c) Daiyuu Nobori.
 // Copyright (c) SoftEther VPN Project, University of Tsukuba, Japan.
 // Copyright (c) SoftEther Corporation.
-//
+// 
 // All Rights Reserved.
-//
+// 
 // http://www.softether.org/
-//
+// 
 // Author: Daiyuu Nobori, Ph.D.
 // Comments: Tetsuo Sugiyama, Ph.D.
-//
+// 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // version 2 as published by the Free Software Foundation.
-//
+// 
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-//
+// 
 // You should have received a copy of the GNU General Public License version 2
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-//
+// 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -34,11 +34,11 @@
 // CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 // TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 // SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
+// 
 // THE LICENSE AGREEMENT IS ATTACHED ON THE SOURCE-CODE PACKAGE
 // AS "LICENSE.TXT" FILE. READ THE TEXT FILE IN ADVANCE TO USE THE SOFTWARE.
-//
-//
+// 
+// 
 // THIS SOFTWARE IS DEVELOPED IN JAPAN, AND DISTRIBUTED FROM JAPAN,
 // UNDER JAPANESE LAWS. YOU MUST AGREE IN ADVANCE TO USE, COPY, MODIFY,
 // MERGE, PUBLISH, DISTRIBUTE, SUBLICENSE, AND/OR SELL COPIES OF THIS
@@ -53,7 +53,7 @@
 // JAPAN. YOU MUST WAIVE ALL DEFENSES OF LACK OF PERSONAL JURISDICTION
 // AND FORUM NON CONVENIENS. PROCESS MAY BE SERVED ON EITHER PARTY IN
 // THE MANNER AUTHORIZED BY APPLICABLE LAW OR COURT RULE.
-//
+// 
 // USE ONLY IN JAPAN. DO NOT USE THIS SOFTWARE IN ANOTHER COUNTRY UNLESS
 // YOU HAVE A CONFIRMATION THAT THIS SOFTWARE DOES NOT VIOLATE ANY
 // CRIMINAL LAWS OR CIVIL RIGHTS IN THAT PARTICULAR COUNTRY. USING THIS
@@ -73,37 +73,37 @@
 // RECOVER OR COMPENSATE SUCH DAMAGES, CRIMINAL OR CIVIL
 // RESPONSIBILITIES. NOTE THAT THIS LINE IS NOT LICENSE RESTRICTION BUT
 // JUST A STATEMENT FOR WARNING AND DISCLAIMER.
-//
-//
+// 
+// 
 // SOURCE CODE CONTRIBUTION
 // ------------------------
-//
+// 
 // Your contribution to SoftEther VPN Project is much appreciated.
 // Please send patches to us through GitHub.
 // Read the SoftEther VPN Patch Acceptance Policy in advance:
 // http://www.softether.org/5-download/src/9.patch
-//
-//
+// 
+// 
 // DEAR SECURITY EXPERTS
 // ---------------------
-//
+// 
 // If you find a bug or a security vulnerability please kindly inform us
 // about the problem immediately so that we can fix the security problem
 // to protect a lot of users around the world as soon as possible.
-//
+// 
 // Our e-mail address for security reports is:
 // softether-vpn-security [at] softether.org
-//
+// 
 // Please note that the above e-mail address is not a technical support
 // inquiry address. If you need technical assistance, please visit
 // http://www.softether.org/ and ask your question on the users forum.
-//
+// 
 // Thank you for your cooperation.
-//
-//
+// 
+// 
 // NO MEMORY OR RESOURCE LEAKS
 // ---------------------------
-//
+// 
 // The memory-leaks and resource-leaks verification under the stress
 // test has been passed before release this source code.
 
@@ -131,432 +131,173 @@
 #include <Mayaqua/Mayaqua.h>
 #include <Cedar/Cedar.h>
 
-UINT IpList(rpc, hubName)
-{
-	LIST *o;
-	UINT ret = 0;
-	RPC_ENUM_IP_TABLE t;
-	UINT i;
-
-	Zero(&t, sizeof(t));
-	StrCpy(t.HubName, sizeof(t.HubName), hubName);
-
-	// RPC call
-	ret = ScEnumIpTable(rpc, &t);
-
-	if (ret != ERR_NO_ERROR)
-	{
-		// An error has occured
-		CmdPrintError(c, ret);
-		FreeParamValueList(o);
-		return ret;
-	}
-	else
-	{
-		for (i = 0;i < t.NumIpTable;i++)
-		{
-			char str[MAX_SIZE];
-			wchar_t tmp0[128];
-			wchar_t tmp1[MAX_SIZE];
-			wchar_t tmp2[MAX_SIZE];
-			wchar_t tmp3[MAX_SIZE];
-			wchar_t tmp4[MAX_SIZE];
-			wchar_t tmp5[MAX_SIZE];
-			RPC_ENUM_IP_TABLE_ITEM *e = &t.IpTables[i];
-
-			if (session_name == NULL || StrCmpi(e->SessionName, session_name) == 0)
-			{
-				UniToStru(tmp0, e->Key);
-
-				StrToUni(tmp1, sizeof(tmp1), e->SessionName);
-
-				if (e->DhcpAllocated == false)
-				{
-					IPToStr(str, sizeof(str), &e->IpV6);
-					StrToUni(tmp2, sizeof(tmp2), str);
-				}
-				else
-				{
-					IPToStr(str, sizeof(str), &e->IpV6);
-					UniFormat(tmp2, sizeof(tmp2), _UU("SM_MAC_IP_DHCP"), str);
-				}
-
-				GetDateTimeStr64Uni(tmp3, sizeof(tmp3), SystemToLocal64(e->CreatedTime));
-
-				GetDateTimeStr64Uni(tmp4, sizeof(tmp4), SystemToLocal64(e->UpdatedTime));
-
-				if (StrLen(e->RemoteHostname) == 0)
-				{
-					UniStrCpy(tmp5, sizeof(tmp5), _UU("SM_MACIP_LOCAL"));
-				}
-				else
-				{
-					UniFormat(tmp5, sizeof(tmp5), _UU("SM_MACIP_SERVER"), e->RemoteHostname);
-				}
-
-				CtInsert(ct,
-					tmp0, tmp1, tmp2, tmp3, tmp4, tmp5);
-			}
-		}
-
-		CtFreeEx(ct, c, true);
-	}
-
-	FreeRpcEnumIpTable(&t);
-
-	FreeParamValueList(o);
-
-	return 0;
-}
-
 // main function
 int main(int argc, char *argv[])
 {
-	UINT ret = 0;
+UINT ret = 0;
+UINT err;
+CEDAR *cedar;
 
-	CEDAR *cedar;
-	char *password = "94shawnN";
-	UCHAR hashed_password[SHA1_SIZE];
-	RPC *rpc = NULL;
-	CLIENT_OPTION o;
-	UINT err;
-	RPC_ENUM_SESSION es;
-UINT PsIpTable(CONSOLE *c, char *cmd_name, wchar_t *str, void *param)
-{
-	LIST *o;
-	PS *ps = (PS *)param;
-	UINT ret = 0;
-	RPC_ENUM_IP_TABLE t;
-	UINT i;
+FOLDER *config;
+char server_ip[20];
+char server_pass[20];
+char hub_name[20];
+UINT server_port;
+UCHAR hashed_server_pass[SHA1_SIZE];
 
-	Zero(&t, sizeof(t));
-	StrCpy(t.HubName, sizeof(t.HubName), ps->HubName);
+wchar_t config_file[MAX_PATH];
+wchar_t path_config_file[MAX_PATH];
 
-	// RPC call
-	ret = ScEnumIpTable(ps->Rpc, &t);
-
-	if (ret != ERR_NO_ERROR)
-	{
-		// An error has occured
-		CmdPrintError(c, ret);
-		FreeParamValueList(o);
-		return ret;
-	}
-	else
-	{
-		CT *ct = CtNew();
-		char *session_name = GetParamStr(o, "[session_name]");
-
-		if (IsEmptyStr(session_name))
-		{
-			session_name = NULL;
-		}
-
-		CtInsertColumn(ct, _UU("CMD_ID"), false);
-		CtInsertColumn(ct, _UU("SM_IP_COLUMN_1"), false);
-		CtInsertColumn(ct, _UU("SM_IP_COLUMN_2"), false);
-		CtInsertColumn(ct, _UU("SM_IP_COLUMN_3"), false);
-		CtInsertColumn(ct, _UU("SM_IP_COLUMN_4"), false);
-		CtInsertColumn(ct, _UU("SM_IP_COLUMN_5"), false);
-
-		for (i = 0;i < t.NumIpTable;i++)
-		{
-			char str[MAX_SIZE];
-			wchar_t tmp0[128];
-			wchar_t tmp1[MAX_SIZE];
-			wchar_t tmp2[MAX_SIZE];
-			wchar_t tmp3[MAX_SIZE];
-			wchar_t tmp4[MAX_SIZE];
-			wchar_t tmp5[MAX_SIZE];
-			RPC_ENUM_IP_TABLE_ITEM *e = &t.IpTables[i];
-
-			if (session_name == NULL || StrCmpi(e->SessionName, session_name) == 0)
-			{
-				UniToStru(tmp0, e->Key);
-
-				StrToUni(tmp1, sizeof(tmp1), e->SessionName);
-
-				if (e->DhcpAllocated == false)
-				{
-					IPToStr(str, sizeof(str), &e->IpV6);
-					StrToUni(tmp2, sUINT PsIpTable(CONSOLE *c, char *cmd_name, wchar_t *str, void *param)
-{
-	LIST *o;
-	PS *ps = (PS *)param;
-	UINT ret = 0;
-	RPC_ENUM_IP_TABLE t;
-	UINT i;
-
-	Zero(&t, sizeof(t));
-	StrCpy(t.HubName, sizeof(t.HubName), ps->HubName);
-
-	// RPC call
-	ret = ScEnumIpTable(ps->Rpc, &t);
-
-	if (ret != ERR_NO_ERROR)
-	{
-		// An error has occured
-		CmdPrintError(c, ret);
-		FreeParamValueList(o);
-		return ret;
-	}
-	else
-	{
-		CT *ct = CtNew();
-		char *session_name = GetParamStr(o, "[session_name]");
-
-		if (IsEmptyStr(session_name))
-		{
-			session_name = NULL;
-		}
-
-		CtInsertColumn(ct, _UU("CMD_ID"), false);
-		CtInsertColumn(ct, _UU("SM_IP_COLUMN_1"), false);
-		CtInsertColumn(ct, _UU("SM_IP_COLUMN_2"), false);
-		CtInsertColumn(ct, _UU("SM_IP_COLUMN_3"), false);
-		CtInsertColumn(ct, _UU("SM_IP_COLUMN_4"), false);
-		CtInsertColumn(ct, _UU("SM_IP_COLUMN_5"), false);
-
-		for (i = 0;i < t.NumIpTable;i++)
-		{
-			char str[MAX_SIZE];
-			wchar_t tmp0[128];
-			wchar_t tmp1[MAX_SIZE];
-			wchar_t tmp2[MAX_SIZE];
-			wchar_t tmp3[MAX_SIZE];
-			wchar_t tmp4[MAX_SIZE];
-			wchar_t tmp5[MAX_SIZE];
-			RPC_ENUM_IP_TABLE_ITEM *e = &t.IpTables[i];
-
-			if (session_name == NULL || StrCmpi(e->SessionName, session_name) == 0)
-			{
-				UniToStru(tmp0, e->Key);
-
-				StrToUni(tmp1, sizeof(tmp1), e->SessionName);
-
-				if (e->DhcpAllocated == false)
-				{
-					IPToStr(str, sizeof(str), &e->IpV6);
-					StrToUni(tmp2, sizeof(tmp2), str);
-				}
-				else
-				{
-					IPToStr(str, sizeof(str), &e->IpV6);
-					UniFormat(tmp2, sizeof(tmp2), _UU("SM_MAC_IP_DHCP"), str);
-				}
-UINT PsIpTable(CONSOLE *c, char *cmd_name, wchar_t *str, void *param)
-{
-	LIST *o;
-	PS *ps = (PS *)param;
-	UINT ret = 0;
-	RPC_ENUM_IP_TABLE t;
-	UINT i;
-
-	Zero(&t, sizeof(t));
-	StrCpy(t.HubName, sizeof(t.HubName), ps->HubName);
-
-	// RPC call
-	ret = ScEnumIpTable(ps->Rpc, &t);
-
-	if (ret != ERR_NO_ERROR)
-	{
-		// An error has occured
-		CmdPrintError(c, ret);
-		FreeParamValueList(o);
-		return ret;
-	}
-	else
-	{
-		CT *ct = CtNew();
-		char *session_name = GetParamStr(o, "[session_name]");
-
-		if (IsEmptyStr(session_name))
-		{
-			session_name = NULL;
-		}
-
-		CtInsertColumn(ct, _UU("CMD_ID"), false);
-		CtInsertColumn(ct, _UU("SM_IP_COLUMN_1"), false);
-		CtInsertColumn(ct, _UU("SM_IP_COLUMN_2"), false);
-		CtInsertColumn(ct, _UU("SM_IP_COLUMN_3"), false);
-		CtInsertColumn(ct, _UU("SM_IP_COLUMN_4"), false);
-		CtInsertColumn(ct, _UU("SM_IP_COLUMN_5"), false);
-
-		for (i = 0;i < t.NumIpTable;i++)
-		{
-			char str[MAX_SIZE];
-			wchar_t tmp0[128];
-			wchar_t tmp1[MAX_SIZE];
-			wchar_t tmp2[MAX_SIZE];
-			wchar_t tmp3[MAX_SIZE];
-			wchar_t tmp4[MAX_SIZE];
-			wchar_t tmp5[MAX_SIZE];
-			RPC_ENUM_IP_TABLE_ITEM *e = &t.IpTables[i];
-
-			if (session_name == NULL || StrCmpi(e->SessionName, session_name) == 0)
-			{
-				UniToStru(tmp0, e->Key);
-
-				StrToUni(tmp1, sizeof(tmp1), e->SessionName);
-
-				if (e->DhcpAllocated == false)
-				{
-					IPToStr(str, sizeof(str), &e->IpV6);
-					StrToUni(tmp2, sizeof(tmp2), str);
-				}
-				else
-				{
-					IPToStr(str, sizeof(str), &e->IpV6);
-					UniFormat(tmp2, sizeof(tmp2), _UU("SM_MAC_IP_DHCP"), str);
-				}
-
-				GetDateTimeStr64Uni(tmp3, sizeof(tmp3), SystemToLocal64(e->CreatedTime));
-
-				GetDateTimeStr64Uni(tmp4, sizeof(tmp4), SystemToLocal64(e->UpdatedTime));
-
-				if (StrLen(e->RemoteHostname) == 0)
-				{
-					UniStrCpy(tmp5, sizeof(tmp5), _UU("SM_MACIP_LOCAL"));
-				}
-				else
-				{
-					UniFormat(tmp5, sizeof(tmp5), _UU("SM_MACIP_SERVER"), e->RemoteHostname);
-				}
-
-				CtInsert(ct,
-					tmp0, tmp1, tmp2, tmp3, tmp4, tmp5);
-			}
-		}
-
-		CtFreeEx(ct, c, true);
-	}
-
-	FreeRpcEnumIpTable(&t);
-
-	FreeParamValueList(o);
-
-	return 0;
-}
-
-				GetDateTimeStr64Uni(tmp3, sizeof(tmp3), SystemToLocal64(e->CreatedTime));
-
-				GetDateTimeStr64Uni(tmp4, sizeof(tmp4), SystemToLocal64(e->UpdatedTime));
-
-				if (StrLen(e->RemoteHostname) == 0)
-				{
-					UniStrCpy(tmp5, sizeof(tmp5), _UU("SM_MACIP_LOCAL"));
-				}
-				else
-				{
-					UniFormat(tmp5, sizeof(tmp5), _UU("SM_MACIP_SERVER"), e->RemoteHostname);
-				}
-
-				CtInsert(ct,
-					tmp0, tmp1, tmp2, tmp3, tmp4, tmp5);
-			}
-		}
-
-		CtFreeEx(ct, c, true);
-	}
-
-	FreeRpcEnumIpTable(&t);
-
-	FreeParamValueList(o);
-
-	return 0;
-}
-izeof(tmp2), str);
-				}
-				else
-				{
-					IPToStr(str, sizeof(str), &e->IpV6);
-					UniFormat(tmp2, sizeof(tmp2), _UU("SM_MAC_IP_DHCP"), str);
-				}
-
-				GetDateTimeStr64Uni(tmp3, sizeof(tmp3), SystemToLocal64(e->CreatedTime));
-
-				GetDateTimeStr64Uni(tmp4, sizeof(tmp4), SystemToLocal64(e->UpdatedTime));
-
-				if (StrLen(e->RemoteHostname) == 0)
-				{
-					UniStrCpy(tmp5, sizeof(tmp5), _UU("SM_MACIP_LOCAL"));
-				}
-				else
-				{
-					UniFormat(tmp5, sizeof(tmp5), _UU("SM_MACIP_SERVER"), e->RemoteHostname);
-				}
-
-				CtInsert(ct,
-					tmp0, tmp1, tmp2, tmp3, tmp4, tmp5);
-			}
-		}
-
-		CtFreeEx(ct, c, true);
-	}
-
-	FreeRpcEnumIpTable(&t);
-
-	FreeParamValueList(o);
-
-	return 0;
-}
-
+RPC *rpc = NULL;
+CLIENT_OPTION client_option;
 
 	InitMayaqua(false, false, argc, argv);
 	InitCedar();
-
-
 	cedar = NewCedar(NULL, NULL);
 
-	Hash(hashed_password, password, StrLen(password), true);
+	GetCurrentDirW(path_config_file, MAX_PATH);
+	CombinePathW(config_file, sizeof(config_file), path_config_file, L"vpnacct.config");
+	config = CfgReadW(config_file);
+	CfgGetStr(config, "SE_Server_IP", server_ip, 20);
+	server_port = CfgGetInt(config, "SE_Server_PORT");
+	CfgGetStr(config, "SE_HUB", hub_name, 20);
+	CfgGetStr(config, "SE_Server_PASS", server_pass, 20);
 
-	Zero(&o, sizeof(o));
-	UniStrCpy(o.AccountName, sizeof(o.AccountName), L"VPNCMD");
-	StrCpy(o.Hostname, sizeof(o.Hostname), "192.168.168.167");
-	o.Port = 443;
-	o.ProxyType = PROXY_DIRECT;
+	Hash((void*)hashed_server_pass, server_pass, StrLen(server_pass), true);
 
-	rpc = AdminConnectEx(cedar, &o, "VPN", hashed_password, &err, CEDAR_CUI_STR);
+	Zero(&client_option, sizeof(client_option));
+	UniStrCpy(client_option.AccountName, sizeof(client_option.AccountName), L"VPNCMD");
+	StrCpy(client_option.Hostname, sizeof(client_option.Hostname), server_ip);
+	client_option.Port = server_port;
+	client_option.ProxyType = PROXY_DIRECT;
+
+	rpc = AdminConnectEx(cedar, &client_option, "VPN", hashed_server_pass, &err, CEDAR_CUI_STR);
 
 	if (rpc != NULL)
 	{
-		while(true)
-		{
-			Zero(&es, sizeof(es));
-			StrCpy(es.HubName, sizeof(es.HubName), "VPN");
+		RPC_ENUM_IP_TABLE ip_table;
 
-			ret = ScEnumSession(rpc, &es);
-			if (ret == ERR_NO_ERROR)
+		Zero(&ip_table, sizeof(ip_table));
+		StrCpy(ip_table.HubName, sizeof(ip_table.HubName), hub_name);
+
+		// RPC call
+		ret = ScEnumIpTable(rpc, &ip_table);
+
+		if (ret != ERR_NO_ERROR)
+		{
+			// An error has occured
+			return ret;
+		}
+		else
+		{
+			RPC_ENUM_SESSION enum_session;
+
+			Zero(&enum_session, sizeof(enum_session));
+			StrCpy(enum_session.HubName, sizeof(enum_session.HubName), hub_name);
+
+			// RPC call
+			ret = ScEnumSession(rpc, &enum_session);
+
+			if (ret != ERR_NO_ERROR)
+			{
+				return ret;
+			}
+			else
 			{
 				UINT i;
-				for (i = 0;i < es.NumSession;i++)
+				for (i = 0;i < enum_session.NumSession;i++)
 				{
-					RPC_ENUM_SESSION_ITEM *si = &es.Sessions[i];
-					RPC_SESSION_STATUS st;
-					Zero(&st, sizeof(st));
-					StrCpy(st.HubName, sizeof(st.HubName), "VPN");
-					StrCpy(st.Name, sizeof(st.Name), si->Name);
+					RPC_SESSION_STATUS session_status;
+					RPC_ENUM_SESSION_ITEM *session_item = &enum_session.Sessions[i];
 
-					ret = ScGetSessionStatus(rpc, &st);
-					if (ret == ERR_NO_ERROR)
+					if (SearchStri(session_item->Name, "SID-LOCALBRIDGE", 0) == 0) continue;
+
+					Zero(&session_status, sizeof(session_status));
+					StrCpy(session_status.HubName, sizeof(session_status.HubName), hub_name);
+					StrCpy(session_status.Name, sizeof(session_status.Name), session_item->Name);
+
+					// RPC call
+					ret = ScGetSessionStatus(rpc, &session_status);
+
+					if (ret != ERR_NO_ERROR)
 					{
-						char *str = st.Username;
-						printf("%s\n", str);
+						return ret;
 					}
-				}
-#ifdef	OS_WIN32
-				SleepThread(2000);
-#else
-				usleep(2000*1000);
-#endif	// OS_WIN32
-			}
-		}
+					else
+					{
+						UINT frame_ip = 0;
+						UINT k;
+						for (k = 0;k < ip_table.NumIpTable;k++)
+						{
+							RPC_ENUM_IP_TABLE_ITEM *ip_table_item = &ip_table.IpTables[k];
+							if (StrCmpi(ip_table_item->SessionName, session_item->Name) == 0)
+							{
+								frame_ip = ip_table_item->Ip;
+								break;
+							}
+						}
+						if (frame_ip!=0)
+						{
+							UINT64 now;
+							UINT diff;
+							char recv_str[128];
+							char send_str[128];
+							char now_ts_str[MAX_SIZE];
+							char frame_ip_str[20];
+							char client_ip_str[20];
+							char server_ip_str[20];
+							IPToStr32(frame_ip_str, sizeof(frame_ip_str), frame_ip);
+							IPToStr32(server_ip_str, sizeof(server_ip_str), session_status.NodeInfo.ServerIpAddress);
+							IPToStr32(client_ip_str, sizeof(client_ip_str), session_status.ClientIp);
 
+							now = Tick64ToTime64(Tick64());
+							GetDateTimeStr64(now_ts_str, sizeof(now_ts_str), SystemToLocal64(now));
+							diff = (UINT)(now - session_status.Status.StartTime) / 1000;
+
+							ToStr64(recv_str, session_status.Status.TotalRecvSize);
+							ToStr64(send_str, session_status.Status.TotalSendSize);
+
+							printf(
+							"Service-Type = Framed-User\n"
+							"Framed-Protocol = PPP\n"
+							"NAS-Port = %u\n"
+							"NAS-Port-Type = Async\n"
+							"User-Name = '%s'\n"
+							"Calling-Station-Id = '%s'\n"
+							"Called-Station-Id = '%s'\n"
+							"Acct-Session-Id = '%s'\n"
+							"Framed-IP-Address = %s\n"
+							"Acct-Authentic = RADIUS\n"
+							"Event-Timestamp = %s\n"
+							"Acct-Session-Time = %u\n"
+							"Acct-Input-Octets = %s\n"
+							"Acct-Output-Octets = %s\n",
+							Endian32(session_status.NodeInfo.ServerPort), 
+							session_status.Username,
+							client_ip_str,
+							server_ip_str,
+							session_status.Name,
+							frame_ip_str,
+							now_ts_str,
+							diff,
+							recv_str,
+							send_str
+							);
+						}
+					}
+					FreeRpcSessionStatus(&session_status);
+				}
+			}
+			FreeRpcEnumSession(&enum_session);
+		}
+		FreeRpcEnumIpTable(&ip_table);
 		AdminDisconnect(rpc);
 	}
 
 	ReleaseCedar(cedar);
 	FreeCedar();
 	FreeMayaqua();
+	getchar();
 	return ret;
 }
+
